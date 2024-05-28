93fbad2567dc2c07b2988f23959672d590e1af00
Rafael Macchi <rafaelmacchi199@gmail.com>
2024-05-28 08:52:13 -0300

    refactor: use css for refactor styles

--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -1,10 +1,10 @@
+import styled, { css } from "styled-components/native";
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
 
 import { UsersThree } from "phosphor-react-native";
 
 export const Container = styled(TouchableOpacity)`
-  width: 355px;
+  width: 100%;
   height: 90px;
 
   background-color: ${({ theme }) => theme.COLORS.GRAY_500};
@@ -14,15 +14,16 @@ export const Container = styled(TouchableOpacity)`
   align-items: center;
 
   padding: 24px;
-  margin-left: 30px;
   margin-bottom: 12px;
 `;
 
 export const Title = styled.Text`
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  color: ${({ theme }) => theme.COLORS.GRAY_200};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `};
+  
   margin-left: 20px;
 `;
 
