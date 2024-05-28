6a246986d58436dc247981302bd52bedba5bd3ef
Rafael Macchi <rafaelmacchi199@gmail.com>
2024-05-28 08:55:56 -0300

    feat: create Filter, Button Icon, Player Card and Players component

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { Alert, FlatList } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { Container, Form, HeaderList, NumbersOfPlayer } from "./styles";
+
+import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import { Header } from "@components/Header";
+import { Button } from "@components/Button";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { PlayerCard } from "@components/PlayerCard";
+
+type RouteParams = {
+  group: string;
+}
+
+export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
+
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight
+        title={group}
+        subtitle="Adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input
+          placeholder="Nome do participante"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon icon="add" />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayer>
+          {players.length}
+        </NumbersOfPlayer>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard
+            name={item}
+            onRemove={() => { }}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Não há jogadores cadastrados ainda."
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 50 },
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
+      />
+
+    </Container>
+  )
+}
